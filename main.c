a33c7cabc5c396d9f88ee629de684e2ba8d2ec68
Новосадов Петр Андреевич <ip615s21@mwp01.csc.local>
2017-02-13 16:41:31 +0700

    456789097543

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdlib>
 
 int main(){
-printf ("hello-world!");
-printf ("Hello-Den");
-system ("pause");
-return 0;
+	printf ("hello-world!");
+	printf ("Hello-Den");
+	system ("pause");
+	return 0;
 }
