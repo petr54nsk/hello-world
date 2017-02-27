fdedd704738bca78d574320ed7f899b7b4ecdcc5
Новосадов Петр Андреевич <ip615s21@mwp01.csc.local>
2017-02-27 16:43:43 +0700

    Hellot43

--- Laba1.cpp
+++ Laba1.cpp
@@ -1,32 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-#include <conio.h>
 #include <iostream>
+using namespace std;
 int main(){
 int d,p,s;
 int k=true;
-	printf ("¬ведите срок вклада: ");
-	std::cin>>d;
-		system("cls");
+	printf ("Enter the number of days of contribution: ");
+	cin>>d;
 		if (d>365 or d<0){
 			while (k==true){
-				system("cls");
-			printf ("¬ведите срок вклада не более 365 дней: ");
-			 	std::cin>>d;
+			printf ("Enter the number of days, not more than 365: ");
+			 	cin>>d;
 					if (d<=365 && d>0){
 						break;
 					}
 				}
 			}
-	printf ("¬ведите сумму вклада: ");
-	std::cin>>s;
-		system("cls");
+	printf ("Enter the amount of your deposit: ");
+	cin>>s;
 		if (s<1000){
 			while (k==true){
-				system("cls");
-			printf ("¬ведите сумму не менее 1000р.: ");
-			 	std::cin>>s;
+			printf ("Enter the deposit amount is not less than 1000r.: ");
+			 	cin>>s;
 					if (s>=1000){
 						break;
 					}
@@ -34,23 +30,22 @@ int k=true;
 			}
 			if(d>=0 && d<=30){
 				p=s-s*0.1;
-				printf ("—умма годового дохода: %d р. \n",p);
+				printf ("The final amount of the deposit: %d r. \n",p);
 			}
 			
 			if(d>=31 && d<=120){
 				p=s+s*0.02;
-				printf ("—умма годового дохода: %d р. \n",p);
+				printf ("The final amount of the deposit: %d r. \n",p);
 			}
 			
 			if(d>=121 && d<=240){
 				p=s+s*0.06;
-				printf ("—умма годового дохода: %d р. \n",p);
+				printf ("The final amount of the deposit: %d r. \n",p);
 			}
 			
 			if(d>=241 && d<=365){
 				p=s+s*0.12;
-				printf ("—умма годового дохода: %d р. \n",p);
+				printf ("The final amount of the deposit: %d r. \n",p);
 			}
-system("pause");
 return 0;
 }
--- Laba2.cpp
+++ Laba2.cpp
@@ -1,80 +1,73 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-#include <conio.h>
 #include <iostream>
 int main(){
 int d,p,s;
 int k=true;
-	printf ("¬ведите срок вклада: ");
+	printf ("Enter the number of days of contribution: ");
 	std::cin>>d;
-		system("cls");
 		if (d>365 or d<0){
 			while (k==true){
-				system("cls");
-			printf ("¬ведите срок вклада не более 365 дней: ");
+			printf ("Enter the number of days, not more than 365: ");
 			 	std::cin>>d;
 					if (d<=365 && d>0){
 						break;
 					}
 				}
 			}
-	printf ("¬ведите сумму вклада: ");
+	printf ("Enter the amount of your deposit: ");
 	std::cin>>s;
-		system("cls");
 		if (s<10000){
 			while (k==true){
-				system("cls");
-			printf ("¬ведите сумму не менее 10000р.: ");
+			printf ("Enter the deposit amount is not less than 10000r.: ");
 			 	std::cin>>s;
 					if (s>=10000){
 						break;
 					}
 				}
 			}
-			system("cls");
 		if (s<100000){
 			if(d>=0 && d<=30){
 				p=s-s*0.1;
-				printf ("—умма годового дохода: %d р. \n",p);
+				printf ("The final amount of the deposit: %d р. \n",p);
 			}
 			
 			if(d>=31 && d<=120){
 				p=s+s*0.02;
-				printf ("—умма годового дохода: %d р. \n",p);
+				printf ("The final amount of the deposit: %d r.\n",p);
 			}
 			
 			if(d>=121 && d<=240){
 				p=s+s*0.06;
-				printf ("—умма годового дохода: %d р. \n",p);
+				printf ("The final amount of the deposit: %d r. \n",p);
 			}
 			
 			if(d>=241 && d<=365){
 				p=s+s*0.12;
-				printf ("—умма годового дохода: %d р. \n",p);
+				printf ("The final amount of the deposit: %d r. \n",p);
 			}
 		}
 				else {
 					if(d>=0 && d<=30){
 					p=s-s*0.1;
-					printf ("—умма годового дохода: %d р. \n",p);
+					printf ("The final amount of the deposit: %d r. \n",p);
 				}
 				
 				if(d>=31 && d<=120){
 					p=s+s*0.03;
-					printf ("—умма годового дохода: %d р. \n",p);
+					printf ("The final amount of the deposit: %d r. \n",p);
 				}
 				
 				if(d>=121 && d<=240){
 					p=s+s*0.08;
-					printf ("—умма годового дохода: %d р. \n",p);
+					printf ("The final amount of the deposit: %d r. \n",p);
 				}
 				
 				if(d>=241 && d<=365){
 					p=s+s*0.15;
-					printf ("—умма годового дохода: %d р. \n",p);
+					printf ("The final amount of the deposit: %d r. \n",p);
 				}
 					}
-system("pause");
 return 0;
 }
